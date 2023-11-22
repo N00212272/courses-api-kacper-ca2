c93fe661b29a101730fe4206faea1f01abd98529
Kacper Agatowski <n00212272@iadt.ie>
2023-11-22 20:24:44 +0000

    created a this for each course thats displayed

---
+++ src/components/CourseCard.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom';
+
+const CourseCard = (props) => {
+    return(
+
+        <div className="card w-96 base-content shadow-xl  ">
+            <div className="card-body">
+                <h2 className="card-title">{props.title}</h2>
+                     <p>{props.code}</p>
+                        <div className="card-actions justify-end">
+                            <Link to={`/courses/${props.id}`}><button className="btn btn-primary">More info</button></Link>
+                        </div>
+            </div>
+        </div>
+       
+
+    );
+}
+export default CourseCard;
