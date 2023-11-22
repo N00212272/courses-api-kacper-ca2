f0cb41430bdec740d4b19ced3e2b559bbade8f66
Kacper Agatowski <n00212272@iadt.ie>
2023-11-22 20:23:58 +0000

    added authentication and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import {useState, useEffect} from 'react';
 // import pages
-import Home from './pages/Home';
+import Login from './pages/Login';
+import Home from './pages/Courses/Home';
 
+import SingleCourse from './pages/Courses/Single'
 // import components
 import MyNavBar from './components/MyNavBar';
 
 
 const App = () => {
+  const [auth,setAuth] = useState(false);
+  let routesProtected;
+  useEffect(()=>{
+    if(localStorage.getItem('token')){
+      setAuth(true);
+    }
+  },[])
+  const onAuth = (checked , token) => {
+    setAuth(checked)
+    if(checked){
+      localStorage.setItem('token', token)
+    }
+    else{
+      localStorage.removeItem('token');
+    }
+  }
+  if(auth){
+    routesProtected = (
+    <>
+        <Route path='/home' element={<Home auth={auth}/>} />
+    </>
+    )
+  }
   return (
 
-    <div className='container mx-auto'>
+    
           
     <Router>
-      <MyNavBar/>
-        <Routes>
-          <Route path="/" element={<Home/>} />   
+      <MyNavBar onAuth={onAuth} auth={auth} />
+      <div className='container mx-auto'>
+        <Routes>  
+        
+          <Route path='/' element={<Login auth={auth} onAuth={onAuth}/>} />
+          <Route path='/home' element={<Home auth={auth}/>} />
+          <Route path='/courses/:id' element={<SingleCourse auth={auth}/>} />
       </Routes>
+      </div>
     </Router>
-    </div>
+    
 
   );
 }
