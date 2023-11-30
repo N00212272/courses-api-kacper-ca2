08ac113229a42390541d6f8a6200f8ce9c1d973c
Kacper Agatowski <n00212272@iadt.ie>
2023-11-30 11:00:22 +0000

    created a lecturer card

---
+++ src/components/Lecturers/LecturerCard.js
@@ -0,0 +1,20 @@
+
+import { Link} from 'react-router-dom';
+const LecturerCard = (props) => {
+    return(
+
+        <div className="card w-96 base-content shadow-xl  ">
+            <div className="card-body">
+                <h2 className="card-title">{props.name}</h2>
+                     <p>{props.email}</p>
+                        <div className="card-actions justify-end">
+                            <Link to={`/lecturers/${props.id}`}><button className="btn btn-primary">More info</button></Link>
+                        </div>
+                        
+            </div>
+        </div>
+       
+
+    );
+}
+export default LecturerCard;
