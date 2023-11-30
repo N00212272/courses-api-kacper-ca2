3d8240212c26989c37994a5ec23cfb939e96dc66
Kacper Agatowski <n00212272@iadt.ie>
2023-11-30 11:00:40 +0000

    created a lecture hero to view single lecturers

---
+++ src/components/Lecturers/LecturerHero.js
@@ -0,0 +1,25 @@
+import DeleteBtn from '../DeleteBtn';
+import { useNavigate, Link } from 'react-router-dom';
+
+const LecturerHero = (props) => {
+  const navigate = useNavigate();
+    return(
+
+        <div className="hero min-h-screen bg-base-200 mt-3">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div>
+            <h1 className="text-5xl font-bold mb-3">{props.name}</h1>
+            <p className="py-2"><b>Address: </b>{props.address}</p>
+            <p className="py-2"><b>email: </b>{props.email}</p>
+            <p className="py-2"><b>phone: </b>{props.phone}</p>
+            <ul className='flex'>
+            <li className='mr-2'><DeleteBtn id={props.id} resource="lecturers" deleteCallback={() => navigate('/lecturers/home')} /></li>
+            <li><Link to={`/lecturers/${props.id}/edit`}><button className='btn btn-info text-l'>Edit</button></Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    );
+}
+export default LecturerHero;
