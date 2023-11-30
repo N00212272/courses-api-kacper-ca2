a648bade4138e9ce20404e7945bafc6b8508a0b6
Kacper Agatowski <n00212272@iadt.ie>
2023-11-30 10:57:23 +0000

    added more routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {useState, useEffect} from 'react';
 // import pages
 import Login from './pages/Login';
-import Home from './pages/Courses/Home';
+import Register from './pages/Register';
 
+
+//courses
+import Home from './pages/Courses/Home';
 import SingleCourse from './pages/Courses/Single'
+import CreateCourse from './pages/Courses/Create';
+import EditCourse from './pages/Courses/Edit';
+
+//Lecturers
+import LecturersHome from './pages/Lecturers/Home';
+import LecturersSingle from './pages/Lecturers/Single';
+import LecturersCreate from './pages/Lecturers/Create';
+import LecturersEdit from './pages/Lecturers/Edit';
+//enrolments
+import EnrolmentsHome from './pages/Enrolments/Home';
+import EnrolmentsSingle from './pages/Enrolments/Single';
+import EnrolmentsCreate from './pages/Enrolments/Create';
+import EnrolmentsEdit from './pages/Enrolments/Edit';
 // import components
 import MyNavBar from './components/MyNavBar';
 
@@ -30,6 +46,19 @@ const App = () => {
     routesProtected = (
     <>
         <Route path='/home' element={<Home auth={auth}/>} />
+        <Route path='/courses/:id' element={<SingleCourse auth={auth}/>} />
+        <Route path='/courses/create' element={<CreateCourse auth={auth}/>} />
+        <Route path='/courses/:id/edit' element={<EditCourse auth={auth}/>} />
+
+        <Route path='/lecturers/home' element={<LecturersHome auth={auth}/>} />
+        <Route path='/lecturers/:id' element={<LecturersSingle auth={auth}/>} />
+        <Route path='/lecturers/create' element={<LecturersCreate auth={auth}/>} />
+        <Route path='/lecturers/:id/edit' element={<LecturersEdit auth={auth}/>} />
+
+        <Route path='/enrolments/home' element={<EnrolmentsHome auth={auth}/>} />
+        <Route path='/enrolments/:id' element={<EnrolmentsSingle auth={auth}/>} />
+        <Route path='/enrolments/create' element={<EnrolmentsCreate auth={auth}/>} />
+        <Route path='/enrolments/:id/edit' element={<EnrolmentsEdit auth={auth}/>} />
     </>
     )
   }
@@ -43,8 +72,9 @@ const App = () => {
         <Routes>  
         
           <Route path='/' element={<Login auth={auth} onAuth={onAuth}/>} />
-          <Route path='/home' element={<Home auth={auth}/>} />
-          <Route path='/courses/:id' element={<SingleCourse auth={auth}/>} />
+          <Route path='/register' element={<Register onAuth={onAuth} />} />
+          {routesProtected}
+          
       </Routes>
       </div>
     </Router>
