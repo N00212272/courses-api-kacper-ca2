08a8c2d9d6d533f894d4576cfc3731febd892276
Kacper Agatowski <n00212272@iadt.ie>
2023-11-30 11:06:28 +0000

    created single page for lecturers

---
+++ src/pages/Lecturers/Single.js
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import LecturerHero from "../../components/Lecturers/LecturerHero";
+import axios from "axios";
+
+const Single = () => {
+    const { id } = useParams();
+    const [lecturer, setLecturer] = useState(null);
+
+    let token = localStorage.getItem('token');
+
+    useEffect(()=> {
+        axios
+        .get(`https://college-api.vercel.app/api/lecturers/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            // console.log(response.data.data)
+            setLecturer(response.data.data)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }, [id]);
+    if(!lecturer) return( <div className="flex justify-center items-center h-screen"><span className="loading loading-infinity"></span></div>);
+
+    return(
+        <LecturerHero id={lecturer.id} name={lecturer.name} address={lecturer.address} email={lecturer.email} phone={lecturer.phone}
+         />
+    )
+}
+export default Single;
