fd4464d5c1391a514ad2d6c7e18e3ad3925b7547
Kacper Agatowski <n00212272@iadt.ie>
2023-12-01 13:27:06 +0000

    created enrolment hero

---
+++ src/components/Enrolments/EnrolmentHero.js
@@ -0,0 +1,28 @@
+import DeleteBtn from '../DeleteBtn';
+import { useNavigate, Link } from 'react-router-dom';
+
+const EnrolmentHero = (props) => {
+  const navigate = useNavigate();
+    return(
+     
+        <div className="hero min-h-screen bg-base-200 mt-3">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+        
+          <div>
+            <h1 className="text-5xl font-bold">{props.status}</h1>
+            {/* I had to do obj.values because it wouldnt show anything within this arrays */}
+            <p className="py-4"><b>Course: </b> {Object.values(props.course)}</p>
+            <p className="py-2"><b>Lecturer: </b> {Object.values(props.lecturer)}</p>
+            <p className="py-2"><b>date: </b>{props.date}</p>
+            <p className="py-2"><b>time: </b>{props.time}</p>
+            <ul className='flex'>
+            <li className='mr-2'><DeleteBtn id={props.id} resource="enrolments" deleteCallback={() => navigate('/enrolments/home')} /></li>
+            <li><Link to={`/enrolments/${props.id}/edit`}><button className='btn btn-info text-l'>Edit</button></Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    
+    );
+}
+export default EnrolmentHero;
