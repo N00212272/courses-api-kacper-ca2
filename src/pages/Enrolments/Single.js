2ed9935cf90452ad9adcb69d75fac9d4bfba9342
Kacper Agatowski <n00212272@iadt.ie>
2023-12-01 13:30:05 +0000

    updated enrolment single

--- src/pages/Enrolments/Single.js
+++ src/pages/Enrolments/Single.js
@@ -1,34 +1,36 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import LecturerHero from "../../components/Lecturers/LecturerHero";
-import axios from "axios";
+import EnrolmentHero from "../../components/Enrolments/EnrolmentHero";
+import axios from '../../config/Api';
 
 const Single = () => {
     const { id } = useParams();
-    const [lecturer, setLecturer] = useState(null);
+    const [enrolment, setEnrolment] = useState(null);
 
-    let token = localStorage.getItem('token');
+    
 
     useEffect(()=> {
+        let token = localStorage.getItem('token');
         axios
-        .get(`https://college-api.vercel.app/api/lecturers/${id}`, {
+        .get(`/enrolments/${id}`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         })
         .then(response => {
             // console.log(response.data.data)
-            setLecturer(response.data.data)
+            setEnrolment(response.data.data)
         })
         .catch(err => {
             console.error(err)
         })
     }, [id]);
-    if(!lecturer) return( <div className="flex justify-center items-center h-screen"><span className="loading loading-infinity"></span></div>);
-
+    if(!enrolment) return( <div className="flex justify-center items-center h-screen"><span className="loading loading-infinity"></span></div>);
+  
     return(
-        <LecturerHero id={lecturer.id} name={lecturer.name} address={lecturer.address} email={lecturer.email} phone={lecturer.phone}
+        <EnrolmentHero id={enrolment.id} status={enrolment.status} time={enrolment.time} date={enrolment.date} course={enrolment.course.title} lecturer={enrolment.lecturer.name}
          />
     )
+  
 }
 export default Single;
