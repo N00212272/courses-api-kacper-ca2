029c337bbcab5b8db7818d4df5fc21be20198eb1
Kacper Agatowski <n00212272@iadt.ie>
2023-12-09 18:29:45 +0000

    created a page to show an unrecognised url

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+const PageNotFound = () => {
+    const errorStyle = {
+        color:'red',
+        fontStyle:'bold'
+    }
+    return(
+        <h1 style={errorStyle}>
+            Page Not Found
+        </h1>
+    );
+
+}
+export default PageNotFound;
