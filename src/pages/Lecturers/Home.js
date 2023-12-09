a55b41e20fc0f8ed8d034c39fbdc03d2bba27b2e
Kacper Agatowski <n00212272@iadt.ie>
2023-12-09 18:32:17 +0000

    added search for lecturers by name

--- src/pages/Lecturers/Home.js
+++ src/pages/Lecturers/Home.js
@@ -3,10 +3,12 @@ import axios from '../../config/Api';
 import {useNavigate, Link} from 'react-router-dom'
 import LecturerCard from "../../components/Lecturers/LecturerCard";
 
-const Home = (auth) =>{
+const Home = ({auth,term}) =>{
+  
   const navigate = useNavigate();
   const [lecturers, setLecturers] = useState([]);
-  let token = localStorage.getItem('token')
+  const [searchLecturers, setSearchLecturers] = useState([]);
+  
     
   const removeLecturer = (id) => {
     let updatedLecturers = lecturers.filter((lecturer) => {
@@ -15,9 +17,20 @@ const Home = (auth) =>{
     setLecturers(updatedLecturers)
 }
 
- 
+useEffect(() => {  
+  if(term.length <= 3){
+    setSearchLecturers(lecturers)
+ }
+ else{
+     let lecturerFilter = lecturers.filter((lecturer) => {
+         return lecturer.name.toLowerCase().includes(term.toLowerCase());
+     })
+     setSearchLecturers(lecturerFilter)
+ }
+},[lecturers,term]);
 
   useEffect(()=> {
+    let token = localStorage.getItem('token')
       axios
       .get('/lecturers',{
         headers: {
@@ -27,15 +40,21 @@ const Home = (auth) =>{
       .then(response => {
         // console.log(response.data.data)
           setLecturers(response.data.data)
+        
       })
       .catch(err => {
           console.error(err)
       })
   }, []);
-
+  console.log(searchLecturers)
+  console.log(lecturers)
+  console.log(term)
   if(lecturers.length === 0) return( <div className="flex justify-center items-center h-screen"><span className="loading loading-infinity"></span></div>);
 
-  const lecturersList = lecturers.map((lecturer,i) => {
+  
+
+
+  const lecturersList = searchLecturers.map((lecturer,i) => {
       return (
          <LecturerCard name={lecturer.name} email={lecturer.email} id={lecturer.id} key={i} deleteCallback={removeLecturer} />
       )
@@ -47,7 +66,7 @@ const Home = (auth) =>{
     ):(
       <>
       <Link to={`/lecturers/create`}><a className="btn btn-success text-l ">Create</a></Link>
-      <h1>Lecturers</h1>
+      <h1 className="text-center mb-8 text-3xl font-bold">Lecturers</h1>
        
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
         {lecturersList}
