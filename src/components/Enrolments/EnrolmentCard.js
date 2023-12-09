43499e983507a530aa2d7ce9c57a5f5465369fc9
Kacper Agatowski <n00212272@iadt.ie>
2023-12-09 18:28:05 +0000

    added detail to what each output is

--- src/components/Enrolments/EnrolmentCard.js
+++ src/components/Enrolments/EnrolmentCard.js
@@ -2,19 +2,21 @@
 import { Link} from 'react-router-dom';
 const EnrolmentCard = (props) => {
     return(
-
+        
         <div className="card w-96 base-content shadow-xl  ">
+            
             <div className="card-body">
                 <h2 className="card-title">{props.status}</h2>
-                     <p>{props.course_id}</p>
-                     <p>{props.lecturer_id}</p>
+                     <p><b>Course: </b>{props.course}</p>
+                     <p><b>Lecturer: </b>{props.lecturer}</p>
                         <div className="card-actions justify-end">
                             <Link to={`/enrolments/${props.id}`}><button className="btn btn-primary">More info</button></Link>
                         </div>
                         
             </div>
+         
         </div>
-       
+    
 
     );
 }
