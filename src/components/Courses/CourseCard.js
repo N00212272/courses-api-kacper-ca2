f4c6b717556a378ad1984511d28833c604b3173d
Kacper Agatowski <n00212272@iadt.ie>
2023-12-09 18:27:41 +0000

    added extra detail in card

--- src/components/Courses/CourseCard.js
+++ src/components/Courses/CourseCard.js
@@ -6,7 +6,8 @@ const CourseCard = (props) => {
         <div className="card w-96 base-content shadow-xl  ">
             <div className="card-body">
                 <h2 className="card-title">{props.title}</h2>
-                     <p>{props.points}</p>
+                     <p><b>Points: </b>{props.points}</p>
+                     <p><b>Level: </b>{props.level}</p>
                         <div className="card-actions justify-end">
                             <Link to={`/courses/${props.id}`}><button className="btn btn-primary">More info</button></Link>
                         </div>
